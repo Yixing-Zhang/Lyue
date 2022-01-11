5e53302106ea7cd5ec02b0d39c65b041ef0fc562
Zhang_Yixing <u3544946@connect.hku.hk>
2022-01-11 22:29:28 +0800

    Support Premake.

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Lyue"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Lyue"
+	location "Lyue"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LY_PLATFORM_WINDOWS",
+			"LY_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "LY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LY_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "LY_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Lyue/vendor/spdlog/include",
+		"Lyue/src"
+	}
+
+	links
+	{
+		"Lyue"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LY_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "LY_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "LY_DIST"
+		symbols "On"
